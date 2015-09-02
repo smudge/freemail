547e9473df1678576d1d09589178782cc7a87f91
Smudge <nathan@ngriffith.com>
2015-09-02 16:37:22 -0400

    Add data + query implementation + specs.

--- lib/freemail.rb
+++ lib/freemail.rb
@@ -1,5 +1,18 @@
-require "freemail/version"
+require 'freemail/version'
 
 module Freemail
-  # Your code goes here...
+  DISPOSABLE = Hash[File.readlines('data/disposable.txt').map { |d| [d.strip.downcase] }]
+  FREE = Hash[File.readlines('data/free.txt').map { |d| [d.strip.downcase] }]
+
+  def self.free?(email)
+    FREE.key?(get_domain(email))
+  end
+
+  def self.disposable?(email)
+    DISPOSABLE.key?(get_domain(email))
+  end
+
+  def self.get_domain(email)
+    email.split('@').last.downcase.strip
+  end
 end
--- spec/freemail_spec.rb
+++ spec/freemail_spec.rb
@@ -5,7 +5,41 @@ describe Freemail do
     expect(Freemail::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.free?' do
+    subject { Freemail.free?(email) }
+
+    context 'with a free email' do
+      let(:email) { 'gopackers@gmail.com' }
+      it { is_expected.to be(true) }
+    end
+
+    context 'with a disposable email' do
+      let(:email) { 'metroidfan9000@10minutemail.com' }
+      it { is_expected.to be(false) }
+    end
+
+    context 'with a paid/private email' do
+      let(:email) { 'steve@apple.com' }
+      it { is_expected.to be(false) }
+    end
+  end
+
+  describe '.disposable?' do
+    subject { Freemail.disposable?(email) }
+
+    context 'with a free email' do
+      let(:email) { 'gopackers@gmail.com' }
+      it { is_expected.to be(false) }
+    end
+
+    context 'with a disposable email' do
+      let(:email) { 'metroidfan9000@10minutemail.com' }
+      it { is_expected.to be(true) }
+    end
+
+    context 'with a paid/private email' do
+      let(:email) { 'steve@apple.com' }
+      it { is_expected.to be(false) }
+    end
   end
 end

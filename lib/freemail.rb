0489204ca363fb36021f0a378eff231d2718de2b
pi-chan <xoyip@piyox.info>
2017-06-16 16:53:34 +0900

    Not Hash but Array

--- lib/freemail.rb
+++ lib/freemail.rb
@@ -2,15 +2,15 @@ require 'freemail/version'
 
 module Freemail
   ROOT = File.join(File.dirname(File.expand_path(__FILE__)), '../')
-  DISPOSABLE = Hash[File.readlines("#{ROOT}/data/disposable.txt").map { |d| [d.strip.downcase] }]
-  FREE = Hash[File.readlines("#{ROOT}/data/free.txt").map { |d| [d.strip.downcase] }]
+  DISPOSABLE = File.readlines("#{ROOT}/data/disposable.txt").map { |d| d.strip.downcase }
+  FREE = File.readlines("#{ROOT}/data/free.txt").map { |d| d.strip.downcase }
 
   def self.free?(email)
-    FREE.key?(get_domain(email))
+    FREE.include?(get_domain(email))
   end
 
   def self.disposable?(email)
-    DISPOSABLE.key?(get_domain(email))
+    DISPOSABLE.include?(get_domain(email))
   end
 
   def self.free_or_disposable?(email)
